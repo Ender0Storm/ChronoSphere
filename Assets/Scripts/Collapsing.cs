b94dc0fcaec4517471189f7ed892df64aafa3ba5
dmichaud92 <83687521+dmichaud92@users.noreply.github.com>
2024-03-23 12:28:08 -0400

    ajout Collapsing floor

---
+++ Assets/Scripts/Collapsing.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collapsing : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Renderer rend;
+    private BoxCollider collider;
+    private bool isCollapsing = false;
+    private Vector3 _startPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        collider = GetComponent<BoxCollider>();
+        _startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!isCollapsing)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                isCollapsing = true;
+                StartCoroutine(Collapse());
+            }
+        }
+    }
+
+    IEnumerator Flashing()
+    {
+        rend.enabled = false;
+        yield return new WaitForSeconds(0.25f);
+        rend.enabled = true;
+        yield return new WaitForSeconds(0.25f);
+    }
+    
+    private void GavityEnabled()
+    {
+        rb.useGravity = true;
+        rb.isKinematic = false;
+    }
+
+    private void GravityDisabled()
+    {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+    
+    private void ActivateCollider()
+    {
+        rend.enabled = true;
+        collider.enabled = true;
+    }
+    
+    private void DisactivateCollider()
+    {
+        rend.enabled = false;
+        collider.enabled = false;
+    }
+    
+    IEnumerator Collapse()
+    {
+        yield return new WaitForSeconds(1);
+        yield return StartCoroutine(Flashing());
+        yield return StartCoroutine(Flashing());
+        yield return StartCoroutine(Flashing());
+        GavityEnabled();
+        yield return new WaitForSeconds(2);
+        GravityDisabled();
+        DisactivateCollider();
+        yield return new WaitForSeconds(3);
+        transform.position = _startPos;
+        ActivateCollider();
+        isCollapsing = false;
+    }
+}
