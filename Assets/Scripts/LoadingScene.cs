dcb886e529f41ee9dbdf044b924b54976f691316
dmichaud92 <83687521+dmichaud92@users.noreply.github.com>
2024-03-27 18:58:38 -0400

    Added rooms to LevelOneScene
    Elle ne sont pas encore connecté

---
+++ Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Load the next scene
+            SceneManager.LoadScene("LevelOneScene");
+        }
+    }
+}
